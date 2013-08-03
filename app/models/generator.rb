3c8288c1548e7d3b403c9c2fdc583f13c702fecf
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-08-03 11:07:59 -0500

    additional formatting, method structure change

--- app/models/generator.rb
+++ app/models/generator.rb
@@ -13,12 +13,38 @@ class Generator
 
   def format_tweet
     @tweet_array.map! do |tweet|
-      tweet = tweet.split(" ")
-      tweet.map {|word| word[0] = "" if word[0] == "#" || word[0] == "@" }
-      tweet.delete_if {|word| word.upcase == "RT"}
-      tweet.delete_if {|word| word[0..3] == "http"}
-      tweet.join(" ")
+      @tweet = tweet
+      split_tweet_into_words
+      remove_hashtags_and_ampersands
+      remove_retweet_announcements
+      remove_urls
+      capitalize_first_word_of_tweet!
+      convert_tweet_words_into_array #must be called last
     end
+  end
+
+  def split_tweet_into_words
+    @tweet = @tweet.split(" ")
+  end
+
+  def remove_hashtags_and_ampersands
+    @tweet.map {|word| word[0] = "" if word[0] == "#" || word[0] == "@" }
+  end
+
+  def remove_retweet_announcements
+    @tweet.delete_if {|word| word.upcase == "RT"}
+  end
+
+  def remove_urls
+    @tweet.delete_if {|word| word[0..3] == "http"}
+  end
+
+  def convert_tweet_words_into_array
+    @tweet.join(" ")
+  end
+
+  def capitalize_first_word_of_tweet!
+    @tweet[0].capitalize!
   end
 
 end
