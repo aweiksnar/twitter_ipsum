bae0c8a1ac2995df9a44a5d5dfc579fc4b46a31c
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-08-04 23:13:21 -0500

    new keys in env variables

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,4 @@
 Twitter.configure do |config|
-  config.consumer_key = "zyNbhf3JO20L2wwFzKvw"
-  config.consumer_secret = "LKKhW3KOwHp9OQzL7KmK1vueRZGIkgf1iUUfXfu0CKQ"
-  config.oauth_token = "353091277-NCSjl5Fyd3gUudKo8TN9CMom5Tggrifj0D2k3QAn"
-  config.oauth_token_secret = "DWZ7y3y13lmnhfOlGYiEZvIm0vox4bMSaKstRVfpCU"
+  config.consumer_key = ENV["twitter_ipsum_consumer_key"]
+  config.consumer_secret = ENV["twitter_ipsum_consumer_secret"]
 end
